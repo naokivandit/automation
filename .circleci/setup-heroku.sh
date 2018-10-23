2e2788e1be9a1c32bfb90145367aea785ce95868
naokivandit <30743722+naokivandit@users.noreply.github.com>
2018-10-23 20:31:08 +0900

    update yml file

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+ wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+ sudo mkdir -p /usr/local/lib /usr/local/bin
+ sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+ cat > ~/.netrc << EOF
+ machine api.heroku.com
+   login n.i.vandit0924@gmail.com
+   password 5314cd80-f0cb-434b-ba8e-899d645ef79d
+ EOF
+
+ cat >> ~/.ssh/config << EOF
+ VerifyHostKeyDNS yes
+ StrictHostKeyChecking no
+ EOF
